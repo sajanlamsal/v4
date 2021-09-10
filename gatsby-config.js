a982a5198c0ca9a05dbc2fe3b7b9b63bddae75cf
saznlamsal <sajan@jobins.jp>
2021-09-10 09:01:40 +0900

    og image changed.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Sajan Lamsal',
-    description: 'Software engineer who specializing in building exceptional digital experiences.',
+    description: 'Software engineer specializing in building exceptional digital experiences.',
     siteUrl: 'https://sazn.dev', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@saznlamsal',
